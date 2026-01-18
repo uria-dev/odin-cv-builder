0a176a94b332eeb904f1d8bd010ba3806b283c83
Uria <uria@urialevy.com>
2026-01-18 17:23:57 +0000

    remove unneeded import

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import LeftSide from "./edit-side.jsx";
-import App from "./App.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LeftSide />
